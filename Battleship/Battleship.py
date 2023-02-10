c23bf5f05ddd82135d9c7a3877860f6e9fde9e0f
Joshua Hodder <godwhyme58@gmail.com>
2023-02-09 23:20:01 -0500

    Stared to Make AI, very buggy

--- Battleship/Battleship.py
+++ Battleship/Battleship.py
@@ -5,6 +5,7 @@
 
 import os
 import msvcrt
+import random
 os.system('cls')
 Start_Try = False
 Vaild_Position = " "
@@ -29,26 +30,56 @@ def Update_Grid(Position, Grid, Change):
     Grid[row][col] = Change #takes the boat position and places it in the grid list
     display_grid(Grid) #displays the grid list
 #Updates the grid after user input
-def pick_boat(Boata, Boatb):
+def Boat_Direction(boat, direction1, direction2):
+    if boat[0] == "a":
+        direction1 = "b"
+    elif boat[0] == "e":
+        direction1 = "d"
+    elif boat[0] == "b":
+        direction1 = "a"
+        direction2 = "c"
+    elif boat[0] == "c":
+        direction1 = "b"
+        direction2 = "d"
+    elif boat[0] == "d":
+        direction1 = "c"
+        direction2 = "e"
+    return direction1, direction2
+
+def AI_Pick_Boat(Boata, Boatb):
     global Vaild_Position
+    Direction1 = ""
+    Direction2 = ""
     Position_1 = int(Boata[1]) - 1
     Position_2 = int(Boata[1]) + 1
     #Sets the second position of the boat part 1
-    if Boata[0] == "a":
-        Direction1 = "b"
-    elif Boata[0] == "e":
-        Direction1 = "d"
-    elif Boata[0] == "b":
-        Direction1 = "a"
-        Direction2 = "c"
-    elif Boata[0] == "c":
-        Direction1 = "b"
-        Direction2 = "d"
-    elif Boata[0] == "d":
-        Direction1 = "c"
-        Direction2 = "e"
+    Direction1, Direction2 = Boat_Direction(Boata, Direction1, Direction2)
+    if Boata[0] in "ae":
+        if Boata[1] == "1":
+            Vaild_Position = [f"{Direction1}1", f"{Boata[0]}2"]
+        elif Boata[1] == "5":
+            Vaild_Position = [f"{Direction1}5", f"{Boata[0]}4"]
+        else:
+            Vaild_Position = [f"{Boata[0]}{Position_1}", f"{Direction1}{Boata[1]}", f"{Boata[0]}{Position_2}"]
+    else:
+        if Boata[1] == "1":
+            Vaild_Position = [f"{Direction1}1", f"{Boata[0]}2", f"{Direction2}1"]
+        elif Boata[1] == "5":
+            Vaild_Position = [f"{Direction1}5", f"{Boata[0]}4", f"{Direction2}5"]
+        else:
+            Vaild_Position = [f"{Direction1}{Boata[1]}", f"{Boata[0]}{Position_1}", f"{Direction2}{Boata[1]}", f"{Boata[0]}{Position_2}"]
+    Boatb = Vaild_Position[random.randrange(0, len(Vaild_Position))]
+
+    return Boatb
+def pick_boat(Boata, Boatb):
+    global Vaild_Position
+    Direction1 = ""
+    Direction2 = ""
+    Position_1 = int(Boata[1]) - 1
+    Position_2 = int(Boata[1]) + 1
+    #Sets the second position of the boat part 1
+    Direction1, Direction2 = Boat_Direction(Boata, Direction1, Direction2)
     #Sets the second position of the boat part 2
-    
     if Boata[0] in "ae":
         if Boata[1] == "1":
             Boatb = (input(f"Select boat second position ({Direction1}1 or {Boata[0]}2): ")).lower()
@@ -71,15 +102,15 @@ def pick_boat(Boata, Boatb):
             Boatb = (input(f"Select boat second position ({Direction1}{Boata[1]}, {Boata[0]}{Position_1}, {Direction2}{Boata[1]}, {Boata[0]}{Position_2}): ")).lower()
             Vaild_Position = [f"{Direction1}{Boata[1]}", f"{Boata[0]}{Position_1}", f"{Direction2}{Boata[1]}", f"{Boata[0]}{Position_2}"]
     #Next position for the boat in all other positions and checks if it is valid
-    return Boata, Boatb
+    return Boatb
 #ensures that the first position of the boat is valid and stets up the second position of the boat
 def pick_boat2(BoatA, BoatB, BoatA1, BoatB1):
-    BoatA, BoatB = pick_boat(BoatA, BoatB)
+    BoatB = pick_boat(BoatA, BoatB)
     while not BoatB in Vaild_Position:
         BoatB = (input("Please enter a valid position: ")).lower()
     while BoatB == BoatA1 or BoatB == BoatB1:
         BoatB = (input("That position is already in use. Please enter a valid position: ")).lower()
-    return BoatA, BoatB
+    return BoatB
 #ensures that the second position of the boat is valid
 def check_in(boat):
     while not (len(boat) == 2 and boat[0] in "abcde" and boat[1] in "12345"):
@@ -99,7 +130,7 @@ def Boat_Place(Boat1a, Boat1b, Boat2a, Boat2b, player):
     Boat1a = (input(f"Player{player}, Select boat position (a1 - e5): ")).lower()
     Boat1a = check_in(Boat1a)
     Update_Grid(Boat1a, grid, "X")
-    Boat1a, Boat1b = pick_boat(Boat1a, Boat1b)
+    Boat1b = pick_boat(Boat1a, Boat1b)
     while not Boat1b in Vaild_Position:
         Boat1b = (input("Please enter a valid position: ")).lower()
     Update_Grid(Boat1b, grid, "X")
@@ -108,7 +139,7 @@ def Boat_Place(Boat1a, Boat1b, Boat2a, Boat2b, player):
     while Boat2a == Boat1a or Boat2a == Boat1b:
         Boat2a = (input("That position is already in use. Please enter a valid position (a1 - e5): ")).lower()
     Update_Grid(Boat2a, grid, "X")
-    Boat2a, Boat2b = pick_boat2(Boat2a, Boat2b, Boat1a, Boat1b)
+    Boat2b = pick_boat2(Boat2a, Boat2b, Boat1a, Boat1b)
     Update_Grid(Boat2b, grid, "X")
     print(f"Player{player} complete\nPress any key to continue")
     msvcrt.getch()
@@ -142,13 +173,92 @@ def Player_Turn(grid, Score, B1a, B2a, B1b, B2b, Picka, Pickb, playerIt, playerN
         os.system('cls') 
     return grid, Pickb, Score
 #Player Gameplay
+def Single_Player():
+    Boat1a = ""
+    Boat1b = ""
+    Boat2a = ""
+    Boat2b = ""
+    AIB1a = ""
+    AIB1b = ""
+    AIB2a = ""
+    AIB2b = ""
+    PickA = ""
+    AIPickA = ""
+    PickB = []
+    AIPickB = []
+    Score = 0
+    AIScore = 0
+    global Play
+    
+    Boat1a, Boat1b, Boat2a, Boat2b = Boat_Place(Boat1a, Boat1b, Boat2a, Boat2b, "")
+    
+    AIB1a = random.choice("abcde") + str(random.randrange(1,5))
+    AIB1b = AI_Pick_Boat(AIB1a, AIB1b)
+    AIB2a = random.choice("abcde") + str(random.randrange(1,5))
+    while AIB2a == AIB1a or AIB2a == AIB1b:
+        AIB2a = random.choice("abcde") + str(random.randrange(1,5))
+    AIB2b = AI_Pick_Boat(AIB2a, AIB2b)
+    while AIB2b == AIB1a or AIB2b == AIB1b:
+        AIB2b = Vaild_Position[random.randrange(0, len(Vaild_Position))]
+        
+    gridP = []
+    for i in range(5):
+        gridP.append(['-' for i in range(5)])
+    gridAI = []
+    for i in range(5):
+        gridAI.append(['-' for i in range(5)])
+        
+    Play = True
+        
+    while Play:
+        gridP, PickB, Score = Player_Turn(gridP, Score, Boat1a, Boat1b, Boat2a, Boat2b, PickA, PickB, "", "AI")
+        if Play:
+            if AIScore == 1 or AIScore == 3:
+                AIPickA = AI_Pick_Boat(AIPickB[-1], AIPickA)
+                AIPickB.append(AIPickA)
+                while AIPickA in AIPickB:
+                    AIPickA = Vaild_Position[random.randrange(0, len(Vaild_Position))]
+                AIPickB.append(AIPickA)
+            else:
+                AIPickA = random.choice("abcde") + str(random.randrange(1,5))
+                AIPickB.append(AIPickA)
+                while AIPickA in AIPickB:
+                    AIPickA = random.choice("abcde") + str(random.randrange(1,5))
+                AIPickB.append(AIPickA)
+            if AIPickA == Boat1a or AIPickA == Boat1b or AIPickA == Boat2a or AIPickA == Boat2b:
+                Update_Grid(AIPickA, gridAI, "X")
+                AIScore += 1
+                print("AI Hit\nPress any key to continue")
+                msvcrt.getch()
+                os.system('cls')
+            else:
+                Update_Grid(AIPickA, gridAI, "O")
+                print("AI Missed\nPress any key to continue")
+                msvcrt.getch()
+                os.system('cls')
+            if AIScore == 4:
+                print("AI Wins!\nPress any key to continue")
+                Play = False
+                msvcrt.getch()
+                os.system('cls')
+    while not Play:
+        Input = input("Would you like to restart [y/n]\n").lower
+        if Input == "y":
+            start()
+        elif Input == "n":
+            print("Thanks for playing!\nPress any key to exit")
+            msvcrt.getch()
+            exit()
+        else:
+            print("Please enter 'y' or 'n'\nPress any key to try again")
+            msvcrt.getch               
 def Multi_Player():
-    P1_Boat2a = ""
     P1_Boat1a = ""
-    P2_Boat2a = ""
-    P2_Boat1a = ""
+    P1_Boat2a = " "
     P1_Boat1b = ""
     P1_Boat2b = ""
+    P2_Boat1a = ""
+    P2_Boat2a = ""
     P2_Boat1b = ""
     P2_Boat2b = ""
     P1_Picka = ""
@@ -157,8 +267,6 @@ def Multi_Player():
     P2_Pickb = []
     P1_Score = 0
     P2_Score = 0
-    P1_Picka = ""
-    P2_Picka = ""
     global Play
     
     P1_Boat1a, P1_Boat1b, P1_Boat2a, P1_Boat2b = Boat_Place(P1_Boat1a, P1_Boat1b, P1_Boat2a, P1_Boat2b, 1)
@@ -189,7 +297,7 @@ def Multi_Player():
         else:
             print("Please enter 'y' or 'n'\nPress any key to try again")
             msvcrt.getch()
-            continue
+
 #Code for Two Player Version
 def start():
     os.system('cls')
@@ -198,7 +306,6 @@ def start():
         print("Enter 1 to play with AI\nEnter 2 to play with another player")
     else:
         print("Welcome to battleship\nEnter 1 to play with AI\nEnter 2 to play with another player")
-    Mode_Select = input()
     try:
         Mode_Select = int(input())
     except ValueError:
@@ -208,10 +315,12 @@ def start():
         os.system('cls')
         start()
     if Mode_Select == 1:
+        print("Welcome")
         os.system('cls')
         print("Playing with AI\nPress any key to continue")
         msvcrt.getch()
         os.system('cls')
+        Single_Player()
     elif Mode_Select == 2:
         os.system('cls')
         print("Playing with another player\nPress any key to continue")
