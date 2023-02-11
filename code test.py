4efb56db11172657b60ffab82b8b055658916972
Joshua Hodder <godwhyme58@gmail.com>
2023-02-10 23:53:38 -0500

    Progect started, can add students

---
+++ code test.py
@@ -0,0 +1,12 @@
+values = [["test", "test2"], ["check", "check2"], ["final", "final2"]]
+
+for sublist in values:
+    if "final" in sublist:
+      print("it works")
+
+#for index, sublist in enumerate(values):
+#    if "check2" in sublist:
+#      print("WE DID IT")
+#      break
+#print(index)
+
