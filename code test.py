ba63c794e8ef480be2205b71b81c9540561b7dad
Joshua Hodder <godwhyme58@gmail.com>
2023-02-11 13:39:36 -0500

    optimization and fixes, working on displayin lists

--- code test.py
+++ code test.py
@@ -1,9 +1,15 @@
+
 values = [["test", "test2"], ["check", "check2"], ["final", "final2"]]
+string = "test 1space  2paces   3spaces"
+
+
+for i in values:
+    print(i[0])
+
 
 #for sublist in values:
 #    if "check2" in sublist:
 #      print("it works")
-
 
 
 #for index, sublist in enumerate(values):
@@ -11,3 +17,25 @@ values = [["test", "test2"], ["check", "check2"], ["final", "final2"]]
 #      i = index
 #      break
 #print(i)
+
+
+#string = string.replace("  ", " ") #only works for double spaces, can't use + like in re.sub
+
+#import re
+#string = re.sub(' +',' ',string)
+
+#print(string)
+
+
+
+#test = False 
+#def function1_test():
+#    print("function 1")
+#def function2_test():
+#    print("function 2")
+#def test_function(function1, function2):
+#    if test == True:
+#       function1()
+#    else:
+#        function2()    
+#test_function(function1_test, function2_test)
