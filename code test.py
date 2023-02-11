abb9c7adc33b5241fa9bbce4f32a6f8dedb72d9e
Joshua Hodder <godwhyme58@gmail.com>
2023-02-11 01:03:20 -0500

    More Bugfixes, Allows spaces in names

--- code test.py
+++ code test.py
@@ -6,8 +6,8 @@ values = [["test", "test2"], ["check", "check2"], ["final", "final2"]]
 
 
 
-for index, sublist in enumerate(values):
-    if "final" in sublist:
-      i = index
-      break
-print(i)
+#for index, sublist in enumerate(values):
+#    if "final" in sublist:
+#      i = index
+#      break
+#print(i)
