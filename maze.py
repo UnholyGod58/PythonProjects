3d9e30155d95862ab9383bbc39c8bc6ab02dba30
Joshua Hodder <godwhyme58@gmail.com>
2023-03-30 11:05:12 -0400

    .

--- maze.py
+++ maze.py
@@ -11,14 +11,16 @@ pygame.display.set_caption("Da Maze")
 
 def display_text(scale, print, x, y):
     text = pygame.font.Font(None, scale).render(print, True, (255,255,255))
-    screen.blit(text, (x, y))
+    screen.blit(text, ((x - text.get_width()//2), y))
     return text
 
 def display_menu():
     screen.fill((25,25,25))
-    text = display_text(200, "Da Maze", (X_resolution//2 - .get_width()//2), (Y_resolution//3))
-    text = display_text(100, "Play", (X_resolution//2 - .get_width()//2), (Y_resolution//2))
-    rect_play = text.get_rect()
+    display_text(150, "Da Maze", (X_resolution//2), (Y_resolution//4))
+    i = display_text(75, "Play", (X_resolution//2), (Y_resolution//2))
+    rect_play = i.get_rect()
+    i = display_text(75, "Exit", (X_resolution//2), (Y_resolution//1.5))
+    rect_exit = i.get_rect()
     pygame.display.flip()
     while True:
         for event in pygame.event.get():
@@ -28,6 +30,10 @@ def display_menu():
                 if rect_play.collidepoint(event.pos):
                     play()
                     break
+                if rect_exit.collidepoint(event.pos):
+                    print("Mark")
+                    pygame.quit()
+                    exit()
                 
 def play():
     pass
